9d72cb1a9f1af017ec50d9f28d00d8271e65e532
Nam HO (Jack) <118241132+namhokatalon@users.noreply.github.com>
2022-12-22 16:04:45 +0700

    Update deploy-katone-qa.sh

--- deploy-katone-qa.sh
+++ deploy-katone-qa.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 ## Katone version: 1.3.2 / 1.7.2 / 1.9.1 / latest
-kversion="1.3.2"
+kversion="latest"
+
+## Get latest docker images
+if [ "$kversion" = "latest" ]
+then
+	echo "Download latest docker image of Katone"
+	docker rmi license-server:latest
+	mkdir /tmp/kimages/
+	cd /tmp/kimages/ 
+	curl -O https://katalon-one-build.s3.amazonaws.com/op/latest/license-server.zip
+	unzip -o license-server.zip
+	cd license-server
+	docker load < images.tar.gz
+	rm -rf /tmp/kimages/
+fi
+
 
 ## Katone start
 cd /tmp/katalon-platform/katone/${kversion}/license-server/
