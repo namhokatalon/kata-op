1b5862c5755bcf173dbf2082a27cd583285e3383
Nam HO (Jack) <118241132+namhokatalon@users.noreply.github.com>
2023-01-04 11:49:13 +0700

    Create docker-install.sh

---
+++ docker-install.sh
@@ -0,0 +1,5 @@
+# Debian: https://docs.docker.com/engine/install/debian/
+# CentOS: https://docs.docker.com/engine/install/centos/
+# Script:
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
